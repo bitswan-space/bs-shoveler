8226eacadff02af904bee362af632f69e06a2478
Jakub Schier <jakub.schier@gmail.com>
2022-03-24 20:28:42 +0100

    zookeeper module for shoveler

--- bs_shoveler/app.py
+++ bs_shoveler/app.py
@@ -3,6 +3,8 @@ import bspump.lookup
 import bspump.elasticsearch
 import bspump.influxdb
 import asab
+import asab.api
+import asab.zookeeper
 
 import fastkafka
 
@@ -14,40 +16,50 @@ L = logging.getLogger(__name__)
 
 
 class BSShovelerApp(bspump.BSPumpApplication):
-    def __init__(self):
-        super().__init__()
-        self.BSPumpService = self.get_service(
-            "bspump.PumpService"
-        )
+	def __init__(self):
+		super().__init__()
+		self.BSPumpService = self.get_service(
+			"bspump.PumpService"
+		)
 
-        fast_kafka_connection = (
-            fastkafka.FastKafkaConnection(
-                self, "FastKafkaConnection"
-            )
-        )
-        self.BSPumpService.add_connection(
-            fast_kafka_connection
-        )
+		fast_kafka_connection = (
+			fastkafka.FastKafkaConnection(
+				self, "FastKafkaConnection"
+			)
+		)
+		self.BSPumpService.add_connection(
+			fast_kafka_connection
+		)
 
-        if "connection:ElasticSearchConnection" in asab.Config:
-            es_connection = (
-                bspump.elasticsearch.ElasticSearchConnection(
-                    self, "ElasticSearchConnection"
-                )
-            )
-            self.BSPumpService.add_connection(es_connection)
+		if "connection:ElasticSearchConnection" in asab.Config:
+			es_connection = (
+				bspump.elasticsearch.ElasticSearchConnection(
+					self, "ElasticSearchConnection"
+				)
+			)
+			self.BSPumpService.add_connection(es_connection)
 
-        elif "connection:InfluxConnection" in asab.Config:
-            influxdb_connection = (
-                bspump.influxdb.InfluxDBConnection(
-                    self, "InfluxConnection"
-                )
-            )
-            self.BSPumpService.add_connection(influxdb_connection)
+		elif "connection:InfluxConnection" in asab.Config:
+			influxdb_connection = (
+				bspump.influxdb.InfluxDBConnection(
+					self, "InfluxConnection"
+				)
+			)
+			self.BSPumpService.add_connection(influxdb_connection)
 
-        else:
-            L.error("Please add connection(s)")
+		else:
+			L.error("Please add connection(s)")
 
-        self.BSPumpService.add_pipeline(
-            ShovelerPipeline(self, "ShovelerPipeline")
-        )
+		self.BSPumpService.add_pipeline(
+			ShovelerPipeline(self, "ShovelerPipeline")
+		)
+
+		self.add_module(asab.web.Module)
+		self.ASABApiService = asab.api.ApiService(self)
+		self.ASABApiService.initialize_web()
+
+		# Initialize ZooKeeper Service
+		from asab.zookeeper import Module
+		self.add_module(Module)
+		self.ZooKeeperService = self.get_service("asab.ZooKeeperService")
+		self.ASABApiService.initialize_zookeeper(self.ZooKeeperService.DefaultContainer)
